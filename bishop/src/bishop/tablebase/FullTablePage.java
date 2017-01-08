1125acea199dcd9961c8a2295bba14b84ab0080c
lezakpetr <lezakpetr@9bef62f3-871a-4b0b-b5c9-8f60a6d681e4>
2017-01-08 21:47:37 +0000

    direct ByteByffer for tables

--- bishop/src/bishop/tablebase/FullTablePage.java
+++ bishop/src/bishop/tablebase/FullTablePage.java
@@ -1,23 +1,24 @@
 package bishop.tablebase;
 
-import java.util.Arrays;
+import java.nio.ByteBuffer;
+import java.nio.ShortBuffer;
 
 
 public class FullTablePage implements ITablePage {
 	private final long offset;
-	private final short[] results;
+	private final ShortBuffer results;
 	
 	public FullTablePage (final long offset, final int size) {
 		this.offset = offset;
-		this.results = new short[size];
+		this.results = ByteBuffer.allocateDirect(Short.BYTES * size).asShortBuffer();
 	}
 	
 	public int getResult (final long index) {
-		return results[(int) (index - offset)];
+		return results.get((int) (index - offset));
 	}
 	
 	public void setResult (final long index, final int result) {
-		results[(int) (index - offset)] = (short) result;
+		results.put((int) (index - offset), (short) result);
 	}
 	
 	public long getOffset() {
@@ -25,17 +26,19 @@ public class FullTablePage implements ITablePage {
 	}
 
 	public long getSize() {
-		return results.length;
+		return results.capacity();
 	}
 
 	public void read(final ITableIteratorRead it) {
-		for (int i = 0; i < results.length; i++) {
-			results[i] = (short) it.getResult();
+		for (int i = 0; i < results.capacity(); i++) {
+			results.put(i, (short) it.getResult());
 			it.next();
 		}
 	}
 
 	public void clear() {
-		Arrays.fill(results, (short) TableResult.ILLEGAL);
+		for (int i = 0; i < results.capacity(); i++) {
+			results.put(i, (short) TableResult.ILLEGAL);
+		}
 	}
 }
