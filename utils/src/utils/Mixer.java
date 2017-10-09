9178f284c53af7b9a202910a8ea711eca1967698
Petr Ležák <36467964+LezakPetr@users.noreply.github.com>
2017-10-09 21:22:06 +0000

    Tested and improved mixer

--- utils/src/utils/Mixer.java
+++ utils/src/utils/Mixer.java
@@ -1,8 +1,32 @@
 package utils;
 
+
 public class Mixer {
 	
-	private static final long LONG_TO_INT_COEFF = 0xb2c9ae1182d84b7bL;
+	public static final long MIX_COEFF = 0xb2c9ae1182d84b7bL;
+	
+	
+	private static long mixRound (final long x, final long coeff) {
+		// Multiply the number by random odd constant.
+		// This spreads the bits to the up. The lower half of 'x' depended on all bits
+		// of input and it is spreaded to all bits of the upper half of 'y'. So upper half of 'y'
+		// contains mixed bits - every bit of 'y' depends on every bit of the input.
+		// This step is also bijective. Suppose that there are two numbers x1 and x2 so
+		// coeff * x1 = coeff * x2 (mod 2^64)
+		// coeff * x1 - coeff * x2 = k * 2^64
+		// coeff * (x1 - x2) = k * 2^64
+		// Now consider the factors of both sides of the equation. The right side contains at lease
+		// 64 factors 2 so the left side must contain it too to satisfy the equation. The expression
+		// x1 - x2 can contain at most 63 factors 2 because 0 <= x1 < 2^64 and 0 <= x2 < 2^64.
+		// So if the coeff does not contain factor 2 - if it is odd - then the equation
+		// cannot be satisfied. This means that multiplication by odd coeff is bijective mapping.
+		final long y = coeff * x;
+		
+		// Xor two halves of the 'y' - nonlinear step with respect to the multiplication.
+		final long z = y ^ (y >>> 32);
+		
+		return z;
+	}
 	
 	/**
 	 * Mixing long bits with following properties:
@@ -11,32 +35,22 @@ public class Mixer {
 	 * - the mapping is bijective 
 
 	 * @param input input
+	 * @param coeff coeff odd coeff
 	 * @return mixed input
 	 */
-	public static long mixLong (final long input) {
+	public static long mixLongWithCoeff (final long input, final long coeff) {
 		// Xor the upper half of the input to the lower half.
 		// This is bijective mapping because we can undo it by same operation.
 		final long x = input ^ (input >>> 32);
 		
-		// Multiply the number by random odd constant.
-		// This spreads the bits to the up. The lower half of 'x' depended on all bits
-		// of input and it is spreaded to all bits of the upper half of 'y'. So upper half of 'y'
-		// contains mixed bits - every bit of 'y' depends on every bit of the input.
-		// This step is also bijective. Suppose that there are two numbers x1 and x2 so
-		// LONG_TO_INT_COEFF * x1 = LONG_TO_INT_COEFF * x2 (mod 2^64)
-		// LONG_TO_INT_COEFF * x1 - LONG_TO_INT_COEFF * x2 = k * 2^64
-		// LONG_TO_INT_COEFF * (x1 - x2) = k * 2^64
-		// Now consider the factors of both sides of the equation. The right side contains at lease
-		// 64 factors 2 so the left side must contain it too to satisfy the equation. The expression
-		// x1 - x2 can contain at most 63 factors 2 because 0 <= x1 < 2^64 and 0 <= x2 < 2^64.
-		// So if the LONG_TO_INT_COEFF does not contain factor 2 - if it is odd - then the equation
-		// cannot be satisfied. This means that multiplication by odd LONG_TO_INT_COEFF is bijective mapping.
-		final long y = LONG_TO_INT_COEFF * x;
-		
-		// Xor two halves of the 'y' - nonlinear step with respect to the multiplication.
-		final long z =  y ^ (y >>> 32);
-		
-		return z;
+		final long z1 = mixRound(x, coeff);
+		final long z2 = mixRound(z1, coeff);
+				
+		return z2;
+	}
+	
+	public static long mixLong(final long input) {
+		return mixLongWithCoeff(input, MIX_COEFF);
 	}
 	
 	/**
---
+++ utilsTest/src/utilsTest/MixerTest.java
@@ -0,0 +1,115 @@
+package utilsTest;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.function.LongBinaryOperator;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+import utils.Mixer;
+
+public class MixerTest {
+
+	private static final int TEST_COUNT = 1000000;
+	
+	public static long getInverseCoeff(final long coeff) {
+		final BigInteger coeffBig = BigInteger.valueOf(coeff);
+		final BigInteger modulus = BigInteger.ONE.shiftLeft(Long.SIZE);
+		
+		return coeffBig.modInverse(modulus).longValue();
+	}
+	
+	@Test
+	public void testInversibility() {
+		final Random rng = new Random();
+		final long inverseCoeff = getInverseCoeff(Mixer.MIX_COEFF);
+		
+		for (int i = 0; i < TEST_COUNT; i++) {
+			final long input = rng.nextLong();
+			final long mixed = Mixer.mixLong(input);
+			final long reversed = Mixer.mixLongWithCoeff(mixed, inverseCoeff);
+			
+			Assert.assertEquals(input, reversed);
+		}
+	}
+	
+	public void testNonlinearityWithOperation (final LongBinaryOperator op) {
+		final Random rng = new Random();
+		int linearCount = 0;
+		
+		for (int i = 0; i < TEST_COUNT; i++) {
+			final long a = rng.nextLong();
+			final long b = rng.nextLong();
+			final long result1 = Mixer.mixLong(op.applyAsLong(a, b));
+			final long result2 = op.applyAsLong(Mixer.mixLong(a), Mixer.mixLong(b));
+			
+			if (result1 == result2)
+				linearCount++;
+		}
+		
+		Assert.assertTrue(linearCount < TEST_COUNT / 1000);
+	}
+	
+	@Test
+	public void testNonlinearity() {
+		testNonlinearityWithOperation((a, b) -> a + b);
+		testNonlinearityWithOperation((a, b) -> a ^ b);
+	}
+	
+	private static final int COUNTER_BITS = 16;
+	private static final int COUNTER_SIZE = 1 << COUNTER_BITS;
+	private static final int MIN_OUTPUT_SET_SIZE = 41240;   // calculateExpectedOutputSetSize(COUNTER_SIZE)
+	
+	@Test
+	public void testDifussion() {		
+		for (int inputOffset = 0; inputOffset < Long.SIZE; inputOffset += COUNTER_BITS) {
+			for (int outputOffset = 0; outputOffset < Long.SIZE; outputOffset += COUNTER_BITS) {
+				final Set<Long> outputSet = new HashSet<>();
+				
+				for (long i = 0; i < COUNTER_SIZE; i++) {
+					final long input = i << inputOffset;
+					final long output = Mixer.mixLong(input);
+					final long reducedOutput = (output >>> outputOffset) & (COUNTER_SIZE - 1);
+					
+					outputSet.add(reducedOutput);
+				}
+				
+				System.out.println(outputSet.size());
+				Assert.assertTrue(outputSet.size() > MIN_OUTPUT_SET_SIZE);
+			}
+		}
+	}
+	
+	private static int calculateExpectedOutputSetSize(final int counterSize) {
+		final double[] coeffs = IntStream.rangeClosed(0, counterSize).mapToDouble(x -> (double) x / (double) counterSize).toArray();
+		double[] actual = new double[counterSize + 1];   // Probability, that there is index items in the set
+		actual[0] = 1.0;
+		
+		for (int i = 0; i < counterSize; i++) {
+			for (int j = i + 1; j >= 0; j--) {
+				double updated = actual[j] * coeffs[j];
+				
+				if (j > 0)
+					updated += actual[j - 1] * (1.0 - coeffs[j - 1]);
+				
+				actual[j] = updated;
+			}
+		}
+		
+		// Find 1% quantil
+		double sum = 0.0;
+		
+		for (int i = 0; i < counterSize; i++) {
+			sum += actual[i];
+			
+			if (sum > 0.01)
+				return i - 1;
+		}
+		
+		return counterSize;
+	}
+}
