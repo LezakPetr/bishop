3724d6018e3c42f9383c615104cca872ba22fcc1
lezakpetr <lezakpetr@9bef62f3-871a-4b0b-b5c9-8f60a6d681e4>
2018-01-07 10:53:53 +0000

    Optimized range decoder by using floating point division

--- utils/src/range/RangeDecoder.java
+++ utils/src/range/RangeDecoder.java
@@ -49,13 +49,26 @@ public final class RangeDecoder extends RangeBase {
 	public int decodeSymbol (final IProbabilityModel probabilityModel) throws IOException {
 		final long range = high - low;
 		final long origLow = low;
-		final int cdf = (int) (((number - low) << MAX_SYMBOL_BITS) / range);
+		
+		// Original code that operated with longs was:
+		//   final int cdf = (int) (((number - low) << MAX_SYMBOL_BITS) / range);
+		// but double division is much faster:
+		final double cdfNumerator = (number - low) << MAX_SYMBOL_BITS;   // Precise (number - low) < 2^40 < 2^52 and then multiplied by 2^16
+		final double cdfDenominator = range;   // Precise because range < 2^40 < 2^52
+		
+		// The cdf is guaranteed to be lower bound. To obtain greater cdf than correct one the division must be at least
+		// 1 ulp close to the next integer which means that it must be closer that 2^(16 - 52) = 2^(-36). But the difference
+		// between division results is at least 2^MAX_SYMBOL_BITS / MAX_RANGE_WIDTH = 2^16 / 2^40 = 2^(-24).
+		// We still have 36 - 24 = 12 spare bits in double precision. This also guarantees that 0 <= cdf < MAX_SYMBOL_CDF.
+		final int cdf = (int) (cdfNumerator / cdfDenominator);
 
 		// Get symbol and try next ones until we have bigger CDF than number.
 		int symbol = probabilityModel.getSymbolForCdf(cdf);
 		
 		long updatedLow;
 		long updatedHigh = getSymbolLowerBound(probabilityModel, origLow, range, symbol);
+		
+		assert (updatedHigh <= number);
 		
 		do {
 			symbol++;
