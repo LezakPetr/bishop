69a996f1a998870f0cd43abbf02c0bd4337d9278
LezakPetr <>
2018-02-21 06:45:55 +0100

    Updated BtTest so it can run BT2630

--- bishopTests/src/bishopTests/BtTest.java
+++ bishopTests/src/bishopTests/BtTest.java
@@ -6,14 +6,18 @@ public class BtTest extends EloTest {
 	
 	private static final long MAX_TIME = 900000;
 	
+	private final long baseElo;
 	private long totalTime;
+	
+	public BtTest(final long baseElo) {
+		this.baseElo = baseElo;
+	}
 	
 	@Override
 	protected long getMaxTimeForPosition() {
 		return MAX_TIME;
 	}
 	
-
 	@Override
 	protected void initialize() {
 		totalTime = 0;
@@ -29,11 +33,14 @@ public class BtTest extends EloTest {
 
 	@Override
 	protected int calculateElo() {
-		return (int) (2450 - totalTime / 30000);
+		return (int) (baseElo - totalTime / 30000);
 	}
 	
 	public static void main (final String[] args) throws IOException, InterruptedException {
-		final BtTest test = new BtTest();
+		final String base = args[2];
+		final int baseElo = Integer.parseInt(base);
+		
+		final BtTest test = new BtTest(baseElo);
 		test.runTest(args);
 	}
 
