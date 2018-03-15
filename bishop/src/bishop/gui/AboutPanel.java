758f35a6b99d0f12d3b9a43992e247eeb38a1d43
LezakPetr <>
2018-03-15 06:51:02 +0100

    version 1.0.1

--- bishop/src/bishop/gui/AboutPanel.java
+++ bishop/src/bishop/gui/AboutPanel.java
@@ -10,7 +10,7 @@ import bishop.controller.ILocalization;
 @SuppressWarnings("serial")
 public class AboutPanel extends JPanel {
 	
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	
 	private JLabel labelProduct;
 	private JLabel labelCopyright;
