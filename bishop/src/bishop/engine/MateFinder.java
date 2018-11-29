c7233910793d436cd5e23a862f3f544e00a66187
LezakPetr <>
2018-11-29 21:15:57 +0100

    Refactored MateFinder into nodes

--- bishop/src/bishop/engine/MateFinder.java
+++ bishop/src/bishop/engine/MateFinder.java
@@ -19,6 +19,137 @@ import bishop.base.PseudoLegalMoveGenerator;
  * @author Ing. Petr Ležák
  */
 public class MateFinder {
+
+	private class NodeRecord {
+		private final int depth;
+		private final NodeRecord nextRecord;
+		private int nonLosingMoveCount;   // Numbers of moves that does not lead to mate
+		private int losingMoveEvaluation;   // Evaluation of losing moves
+		private final Move killerMove = new Move();
+		private final Move move = new Move();
+		private final Move nonLosingMove = new Move();   // Single moves that does not lead to mate
+
+		public NodeRecord(final int depth, final NodeRecord nextRecord) {
+			this.depth = depth;
+			this.nextRecord = nextRecord;
+		}
+
+		public int findMate(final int horizon, final int extension, final int alpha, final int beta) {
+			nonLosingMoveCount = 0;
+			losingMoveEvaluation = Evaluation.MIN;
+
+			if (position.isKingNotOnTurnAttacked())
+				return Evaluation.MAX;
+
+			if (horizon < 0)
+				return (isMate()) ? -Evaluation.getMateEvaluation(depth + depthAdvance) : Evaluation.DRAW;
+
+			final boolean isAttacker = position.getOnTurn() == attackerColor;
+			boolean mustBeCheck = false;
+
+			if (!isAttacker) {
+				if (!position.isCheck())
+					return Evaluation.DRAW;
+				else
+					mustBeCheck = true;
+			}
+
+			boolean existLegalMove = false;
+			int updatedAlpha = alpha;
+			int effectiveAlpha = alpha;
+			int evaluation = Evaluation.MIN;
+
+			final boolean singularExtensionPossible = extension + 2 <= maxExtension;
+
+			if (killerMove.uncompressMove(killerMoves[depth + depthAdvance], position)) {
+				final int subEvaluation = evaluateMove(horizon, extension, effectiveAlpha, beta, killerMove);
+
+				existLegalMove |= (subEvaluation > Evaluation.MIN);
+				updatedAlpha = Math.max(updatedAlpha, subEvaluation);
+				evaluation = Math.max(evaluation, subEvaluation);
+
+				effectiveAlpha = calculateEffectiveAlpha(alpha, updatedAlpha, isAttacker, singularExtensionPossible);
+
+				if (effectiveAlpha > beta)
+					return subEvaluation;
+			}
+
+			final int moveStackBegin = moveStackTop;
+			moveGenerator.setGenerateOnlyChecks(isAttacker);
+			moveGenerator.setReduceMovesInCheck(!isAttacker);
+			moveGenerator.generateMoves();
+
+			int moveStackEnd = moveStackTop;
+
+			moveStack.sortMoves(moveStackBegin, moveStackEnd);
+
+			while (moveStackEnd > moveStackBegin) {
+				moveStack.getMove(moveStackEnd - 1, move);
+
+				if (!move.equals(killerMove)) {
+					moveStackTop = moveStackEnd;
+					final int subEvaluation = evaluateMove(horizon, extension, effectiveAlpha, beta, move);
+
+					evaluation = Math.max(evaluation, subEvaluation);
+
+					existLegalMove |= (subEvaluation > Evaluation.MIN);
+					updatedAlpha = Math.max(updatedAlpha, subEvaluation);
+
+					effectiveAlpha = calculateEffectiveAlpha(alpha, updatedAlpha, isAttacker, singularExtensionPossible);
+
+					if (effectiveAlpha > beta) {
+						killerMoves[depth + depthAdvance] = move.getCompressedMove();
+						moveStackTop = moveStackBegin;
+						historyTable.addCutoff(position.getOnTurn(), move, horizon);
+
+						return evaluation;
+					}
+				}
+
+				moveStackEnd--;
+			}
+
+			moveStackTop = moveStackBegin;
+
+			if (existLegalMove) {
+				// Singular extension
+				if (!isAttacker && nonLosingMoveCount == 1 && singularExtensionPossible)
+					evaluation = evaluateMove(horizon + 2, extension + 2, losingMoveEvaluation, beta, nonLosingMove);
+
+				return evaluation;
+			}
+			else {
+				if (mustBeCheck || position.isCheck())
+					return -Evaluation.getMateEvaluation(depth + depthAdvance);
+				else
+					return Evaluation.DRAW;
+			}
+		}
+
+		private int calculateEffectiveAlpha (final int originalAlpha, final int updatedAlpha, final boolean isAttacker, final boolean singularExtensionPossible) {
+			if (isAttacker || nonLosingMoveCount > 1 || (depth > 0 && !singularExtensionPossible))
+				return updatedAlpha;
+			else
+				return Math.max(losingMoveEvaluation, originalAlpha);
+		}
+
+		private int evaluateMove(final int horizon, final int extension, int alpha, final int beta, final Move move) {
+			position.makeMove(move);
+
+			final int subHorizon = SerialSearchEngine.matePrunning(depth, horizon - 1, alpha, beta, 1);
+			final int subEvaluation = -nextRecord.findMate(subHorizon, extension, -beta, -alpha);
+			position.undoMove(move);
+
+			if (subEvaluation > -Evaluation.MATE_MIN) {
+				nonLosingMoveCount++;
+				nonLosingMove.assign(move);
+			}
+			else
+				losingMoveEvaluation = Math.max(losingMoveEvaluation, subEvaluation);
+
+			return subEvaluation;
+		}
+	}
 	
 	private int maxDepth;
 	private int maxExtension;
@@ -26,15 +157,9 @@ public class MateFinder {
 	private int moveStackTop;
 	private int[] killerMoves;   // Killer moves for given depth
 	private final HistoryTable historyTable = new HistoryTable();
-	private int[] nonLosingMoveCounts;   // Numbers of moves that does not lead to mate for given depth
-	private int[] losingMovesEvaluations;   // Evaluation of losing moves for given depth
-	private final MoveList nonLosingMoves;   // Single moves that does not lead to mate for given depth
 	private int depthAdvance;
 	private int attackerColor;   // Color of the side that gives mate
-
-	// Pre-created objects to prevent allocation
-	private Move[] precreatedKillerMoves;
-	private Move[] precreatedMoves;
+	private NodeRecord[] nodeRecords;
 	
 	private final IMoveWalker walker = new IMoveWalker() {
 		public boolean processMove(final Move move) {
@@ -54,7 +179,6 @@ public class MateFinder {
 	public MateFinder() {
 		this.moveGenerator = new PseudoLegalMoveGenerator();
 		this.moveGenerator.setWalker(walker);
-		this.nonLosingMoves = new MoveList();
 	}
 	
 	public void setPosition (final Position position) {
@@ -66,125 +190,6 @@ public class MateFinder {
 		return mateChecker.isMate(position);
 	}
 	
-	private int calculateEffectiveAlpha (final int depth, final int originalAlpha, final int updatedAlpha, final boolean isAttacker, final boolean singularExtensionPossible) {
-		if (isAttacker || nonLosingMoveCounts[depth] > 1 || (depth > 0 && !singularExtensionPossible))
-			return updatedAlpha;
-		else
-			return Math.max(losingMovesEvaluations[depth], originalAlpha);
-	}
-	
-	private int findMate(final int depth, final int horizon, final int extension, final int alpha, final int beta) {
-		nonLosingMoveCounts[depth] = 0;
-		losingMovesEvaluations[depth] = Evaluation.MIN;
-
-		if (position.isKingNotOnTurnAttacked())
-			return Evaluation.MAX;
-		
-		if (horizon < 0)
-			return (isMate()) ? -Evaluation.getMateEvaluation(depth + depthAdvance) : Evaluation.DRAW;
-		
-		final boolean isAttacker = position.getOnTurn() == attackerColor;
-		boolean mustBeCheck = false;
-
-		if (!isAttacker) {
-			if (!position.isCheck())
-				return Evaluation.DRAW;
-			else
-				mustBeCheck = true;
-		}
-		
-		final Move killerMove = precreatedKillerMoves[depth];
-		boolean existLegalMove = false;
-		int updatedAlpha = alpha;
-		int effectiveAlpha = alpha;
-		int evaluation = Evaluation.MIN;
-		
-		final boolean singularExtensionPossible = extension + 2 <= maxExtension;
-		
-		if (killerMove.uncompressMove(killerMoves[depth + depthAdvance], position)) {
-			final int subEvaluation = evaluateMove(depth, horizon, extension, effectiveAlpha, beta, killerMove);
-			
-			existLegalMove |= (subEvaluation > Evaluation.MIN);
-			updatedAlpha = Math.max(updatedAlpha, subEvaluation);
-			evaluation = Math.max(evaluation, subEvaluation);
-			
-			effectiveAlpha = calculateEffectiveAlpha(depth, alpha, updatedAlpha, isAttacker, singularExtensionPossible);
-			
-			if (effectiveAlpha > beta)
-				return subEvaluation;
-		}
-		
-		final int moveStackBegin = moveStackTop;
-		moveGenerator.setGenerateOnlyChecks(isAttacker);
-		moveGenerator.setReduceMovesInCheck(!isAttacker);
-		moveGenerator.generateMoves();
-
-		int moveStackEnd = moveStackTop;
-
-		moveStack.sortMoves (moveStackBegin, moveStackEnd);
-		
-		final Move move = precreatedMoves[depth];
-		
-		while (moveStackEnd > moveStackBegin) {
-			moveStack.getMove(moveStackEnd - 1, move);
-			
-			if (!move.equals(killerMove)) {
-				moveStackTop = moveStackEnd;
-				final int subEvaluation = evaluateMove(depth, horizon, extension, effectiveAlpha, beta, move);
-				
-				evaluation = Math.max(evaluation, subEvaluation);
-				
-				existLegalMove |= (subEvaluation > Evaluation.MIN);
-				updatedAlpha = Math.max(updatedAlpha, subEvaluation);
-			
-				effectiveAlpha = calculateEffectiveAlpha(depth, alpha, updatedAlpha, isAttacker, singularExtensionPossible);
-				
-				if (effectiveAlpha > beta) {
-					killerMoves[depth + depthAdvance] = move.getCompressedMove();
-					moveStackTop = moveStackBegin;
-					historyTable.addCutoff(position.getOnTurn(), move, horizon);
-					
-					return evaluation;
-				}
-			}
-
-			moveStackEnd--;
-		}
-		
-		moveStackTop = moveStackBegin;
-		
-		if (existLegalMove) {
-			// Singular extension
-			if (!isAttacker && nonLosingMoveCounts[depth] == 1 && singularExtensionPossible)
-				evaluation = evaluateMove(depth, horizon + 2, extension + 2, losingMovesEvaluations[depth], beta, nonLosingMoves.get(depth));
-			
-			return evaluation;
-		}
-		else {
-			if (mustBeCheck || position.isCheck())
-				return -Evaluation.getMateEvaluation(depth + depthAdvance);
-			else
-				return Evaluation.DRAW;
-		}
-	}
-
-	public int evaluateMove(final int depth, final int horizon, final int extension, int alpha, final int beta, final Move move) {
-		position.makeMove(move);
-		
-		final int subHorizon = SerialSearchEngine.matePrunning(depth, horizon - 1, alpha, beta, 1);
-		final int subEvaluation = -findMate(depth + 1, subHorizon, extension, -beta, -alpha);
-		position.undoMove(move);
-		
-		if (subEvaluation > -Evaluation.MATE_MIN) {
-			nonLosingMoveCounts[depth]++;
-			nonLosingMoves.set(depth, move);
-		}
-		else
-			losingMovesEvaluations[depth] = Math.max(losingMovesEvaluations[depth], subEvaluation);
-		
-		return subEvaluation;
-	}
-	
 	public int findWin(final int depthInMoves) {
 		final int horizon = 2 * depthInMoves - 2;
 		
@@ -194,7 +199,7 @@ public class MateFinder {
 		moveStackTop = 0;
 		attackerColor = position.getOnTurn();
 		
-		return findMate(0, horizon, 0, Evaluation.MATE_MIN, Evaluation.MAX);
+		return nodeRecords[0].findMate(horizon, 0, Evaluation.MATE_MIN, Evaluation.MAX);
 	}
 	
 	/**
@@ -211,28 +216,24 @@ public class MateFinder {
 		moveStackTop = 0;
 		attackerColor = Color.getOppositeColor(position.getOnTurn());
 		
-		return findMate(0, horizon, 0, Evaluation.MIN, -Evaluation.MATE_MIN);
+		return nodeRecords[0].findMate(horizon, 0, Evaluation.MIN, -Evaluation.MATE_MIN);
 	}
 
 	public void setMaxDepth (final int maxDepthInMoves, final int maxDepthAdvance, final int maxExtensionInMoves) {
 		this.maxDepth = 2 * maxDepthInMoves - 1;
 		this.maxExtension = 2 * maxExtensionInMoves;
 		
-		final int maxExtendedDepth = maxDepth + maxExtension;
+		final int maxExtendedDepth = maxDepth + maxExtension + 1;
 		moveStack = new MoveStack((maxExtendedDepth + 1) * PseudoLegalMoveGenerator.MAX_MOVES_IN_POSITION);
 		
 		this.killerMoves = new int[maxExtendedDepth + maxDepthAdvance + 1];
-		this.nonLosingMoveCounts = new int[maxExtendedDepth + 2];
-		this.losingMovesEvaluations = new int[maxExtendedDepth + 2];
-		this.precreatedKillerMoves = new Move[maxExtendedDepth + 1];
-		this.precreatedMoves = new Move[maxExtendedDepth + 1];
+		this.nodeRecords = new NodeRecord[maxExtendedDepth + 1];
+
+		NodeRecord nextRecord = null;
 		
-		this.nonLosingMoves.reserve(maxExtendedDepth + 1);
-		
-		for (int i = 0; i <= maxExtendedDepth; i++) {
-			this.nonLosingMoves.add(new Move());
-			this.precreatedKillerMoves[i] = new Move();
-			this.precreatedMoves[i] = new Move();
+		for (int i = maxExtendedDepth; i >= 0; i--) {
+			this.nodeRecords[i] = new NodeRecord(i, nextRecord);
+			nextRecord = nodeRecords[i];
 		}
 	}
 	
@@ -245,15 +246,15 @@ public class MateFinder {
 	}
 	
 	public int getNonLosingMoveCount() {
-		return nonLosingMoveCounts[0];
+		return nodeRecords[0].nonLosingMoveCount;
 	}
 	
 	public void getNonLosingMove(final Move move) {
-		nonLosingMoves.assignToMove(0, move);
+		move.assign(nodeRecords[0].nonLosingMove);
 	}
 	
 	public int getLosingMovesEvaluation() {
-		return losingMovesEvaluations[0];
+		return nodeRecords[0].losingMoveEvaluation;
 	}
 	
 }
