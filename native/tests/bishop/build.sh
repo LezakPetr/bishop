b469ddae5d63616b2635d79f732e90e8f25d02e0
LezakPetr <PetrLezakCZ@gmail.com>
2019-11-01 21:13:45 +0100

    Removed native code

--- native/tests/bishop/build.sh
+++
@@ -1,8 +0,0 @@
-
-OBJ=`ls ../../bishop/base/*.o`
-
-for cpp in `ls *.cpp`; do
-	bin=`echo $cpp | cut -d "." -f 1`
-	g++ -o $bin $OBJ $cpp
-done
-
--- native/tests/util/build.sh
+++
@@ -1,6 +0,0 @@
-
-for cpp in `ls *.cpp`; do
-	bin=`echo $cpp | cut -d "." -f 1`
-	g++ $cpp -o $bin
-done
-
